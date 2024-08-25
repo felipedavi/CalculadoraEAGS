069bfb9d66772014552d3f393eae8e1bf8418046
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2024-08-25 15:00:21 -0300

    feat[viewBinding]: Configura MainFragment para usar ViewBinding em atividades

--- app/src/main/java/io/github/felipedavi/eags_calculator/MainFragment.kt
+++ app/src/main/java/io/github/felipedavi/eags_calculator/MainFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.github.felipedavi.eags_calculator.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
