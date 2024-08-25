0788a4141f248971d47651b5531d244f822e18ca
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2024-08-25 15:17:40 -0300

    chore[build]: Atualiza versionCode no build.gradle (:app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "io.github.felipedavi.eags_calculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 5
         versionName = "1.0"
     }
 
